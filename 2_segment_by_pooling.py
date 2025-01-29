71fbe16eca3794a359d10c0f89e20464115524cd
Erik <eekihenriksson@gmail.com>
2025-01-29 22:03:26 +0200

    Update 2_segment_by_pooling.py

--- 2_segment_by_pooling.py
+++ 2_segment_by_pooling.py
@@ -74,7 +74,7 @@ class TextSegmenter:
         ) / attention_mask.sum()
         full_text_embedding = full_text_embedding.cpu().numpy().tolist()
 
-        if len(text) < 500:
+        if len(text) < 0:
             return [(text, parent_probs, full_text_embedding)]
 
         sentences = sent_tokenize(text)
@@ -91,7 +91,7 @@ class TextSegmenter:
             segment1 = " ".join(sentences[:split_idx])
             segment2 = " ".join(sentences[split_idx:])
 
-            if len(segment1) >= 250 and len(segment2) >= 250:
+            if len(segment1) >= 0 and len(segment2) >= 0:
                 seg1_end = next(
                     i
                     for i, (_, end) in enumerate(offset_mapping)
