f0be0acc5e7997ea850072032fc1809185a9eb86
Erik <eekihenriksson@gmail.com>
2025-02-04 14:02:32 +0200

    Update 2aaaa_segment_multiscale_opt.py

--- 2aaaa_segment_multiscale_opt.py
+++ 2aaaa_segment_multiscale_opt.py
@@ -23,7 +23,7 @@ class MultiScaleConfig:
 
     def __post_init__(self):
         if self.scale_weights is None:
-            self.scale_weights = {"individual": 1 / 3, "pairs": 1 / 3, "whole": 1 / 3}
+            self.scale_weights = {"individual": 0, "pairs": 0, "whole": 1}
 
 
 class FastMultiScaleSegmenter:
