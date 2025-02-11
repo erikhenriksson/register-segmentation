1084c6075536db1639451f6e710c404926f8c186
Erik <eekihenriksson@gmail.com>
2025-02-11 12:16:28 +0200

    Update 2aab_segment_multiscale_from_embeddings.py

--- 2aab_segment_multiscale_from_embeddings.py
+++ 2aab_segment_multiscale_from_embeddings.py
@@ -439,9 +439,9 @@ class MultiScaleSegmenter:
             return [(text, probs, embedding)]
 
         # Merge short sentences into adequately-sized groups
-        sentences, sent_spans = self.merge_short_sentences(sentences, sent_spans)
+        # sentences, sent_spans = self.merge_short_sentences(sentences, sent_spans)
 
-        segments = self.segment_recursive(text, sentences, sent_spans)
+        # segments = self.segment_recursive(text, sentences, sent_spans)
 
         if len(segments) == 1:
             probs, embedding = self.get_register_probs()
