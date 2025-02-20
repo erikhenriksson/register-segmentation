2d56b5853cda562890391030bdce3808640e292b
Erik <eekihenriksson@gmail.com>
2025-02-20 22:47:42 +0200

    Update x_semantic_embed_data.py

--- x_semantic_embed_data.py
+++ x_semantic_embed_data.py
@@ -104,6 +104,7 @@ def process_data(
         process_batch(batch_texts, batch_records, tokenizer, model, full_text_output)
 
     # Process segments
+    exit()
     logger.info("Processing segments...")
     batch_texts = []
     batch_records = []
